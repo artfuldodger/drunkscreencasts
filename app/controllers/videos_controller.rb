064eece952c4105f500ec93ab897b60708793a26
Jon Evans <jle@foraker.com>
2015-06-18 23:50:23 -0600

    Add video show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,4 +2,8 @@ class VideosController < ApplicationController
   def index
     @videos = Video.ordered
   end
+
+  def show
+    @video = Video.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'videos#index'
+
+  resources :videos, only: [:index, :show]
 end
